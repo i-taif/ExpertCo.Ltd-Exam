5adf1c87dc6a78a62c56df5d7bb1687c07749bf6
t h <taif@gmail.com>
2022-09-27 22:14:10 +0300

    python Task1

--- Python/Task1.py
+++ Python/Task1.py
@@ -1,10 +1,12 @@
-
 from typing import List
 def quicksort(arr: List) -> List:
+    '''
+    this func to sorts it alphabetically withou using sort(), by using recursion
+    '''
     if not arr:
         return []
     return (quicksort([x for x in arr[1:] if x <  arr[0]])
             + [arr[0]] +
             quicksort([x for x in arr[1:] if x >= arr[0]]))
 
-print(quicksort(['c','ad','as','y','p']))
+print(quicksort(['c','ad','as','y','p'])) #output: ['ad', 'as', 'c', 'p', 'y']
